7f1c91a5bca31b3fb9c54e4f89d1f43dc2415818
Hiba-Mohamed <127333097+Hiba-Mohamed@users.noreply.github.com>
2023-03-31 18:58:02 -0230

    Restarted again

--- RPS GAME/function.js
+++ RPS GAME/function.js
@@ -1,43 +1,25 @@
-const game = ()=> {
-    let pScore = 0
-    let cScore = 0
+const playBtn = document.querySelector(".intro button");
+const introScreen = document.querySelector(".intro");
+const match = document.querySelector(".match");
 
-    // start the game
-    const startGame = () => {
-        const playBtn = document.querySelector(".intro button");
-        const introScreen = document.querySelector(".intro");
-        const match = document.querySelector(".match");
-        playBtn.addEventListener("click", () => {
-            introScreen.classList.add("fadeout");
-            match.classList.add("fadeIn");
-        });
+const game = () =>
+    {
+         let pscore = 0;
+         let cscore = 0;
+
+         const startGame = () => 
+             {
+                playBtn.addEventListener
+                ('click', 
+                () => {
+                        introScreen.classList.add("fadeOut");
+                      }
+                );
+             };
+
+            //  call all the inner functions
+                startGame();
     };
 
-// play match
-const playMatch = () => {
-    const options = documeny.querySelectorAll(".options button");
-    const playerHand = document.querySelector(".player-hand");
-    const computerHand = document.querySelector("computer-hand");
-
-    // computer options
-    const computerOptions = ["rock", "paper", "scissors"];
-
-    options.forEach(Option=>{
-options.addEventListener("click", function(){
-    // computer choice
-    const computerNumber = Math.floor(Math.random()*3);
-    const computerChoise = computerNumber[computerNumber]
-    console.log(computerChoise);
-});
-    });
-
-    
-
-    console.log(computerNumber);
-}
-
-    // call all the inner functions
-    startGame();
-};
-// start the game function
-game();
+    // call the game function
+    game(); 
